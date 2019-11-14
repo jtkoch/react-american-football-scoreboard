a16860300efea8e5d4a1e952c3fa22ec8bf7eb84
Jensen Koch <pdpdrums12@Jensens-MacBook-Pro.local>
2019-11-13 21:23:37 -0600

    Changing timer for stretch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
@@ -7,9 +7,32 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [score, setScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
   console.log(score);
   console.log(setScore);
   console.log(useState);
+
+  function toggle() {
+    setIsActive(!isActive);
+  }
+
+  function reset() {
+    setSeconds(0);
+    setIsActive(false);
+  }
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds(seconds => seconds + 1);
+      }, 1000);
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
 
   return (
     <div className="container">
@@ -22,7 +45,17 @@ function App() {
 
             <div className="home__score">{score}</div>
           </div>
-          <div className="timer">00:05</div>
+            <div className="app">
+              <div className="timer">{seconds}</div>
+              <div className="row">
+              <button className={`button button-primary button-primary-${isActive ? 'active' : 'inactive'}`} onClick={toggle}>
+                {isActive ? 'Pause' : 'Start'}
+              </button>
+              <button className="button" onClick={reset}>
+                Reset
+              </button>
+            </div>
+    </div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score">{awayScore}</div>
