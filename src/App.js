b6ea27e7364449c60b00e26c5dd4fe4426d71d5d
Jensen Koch <pdpdrums12@Jensens-MacBook-Pro.local>
2019-12-18 19:43:33 -0600

    redoing project for webpt12 working on MVP

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
@@ -7,32 +7,9 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [score, setScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
-  const [seconds, setSeconds] = useState(0);
-  const [isActive, setIsActive] = useState(false);
   console.log(score);
   console.log(setScore);
   console.log(useState);
-
-  function toggle() {
-    setIsActive(!isActive);
-  }
-
-  function reset() {
-    setSeconds(0);
-    setIsActive(false);
-  }
-
-  useEffect(() => {
-    let interval = null;
-    if (isActive) {
-      interval = setInterval(() => {
-        setSeconds(seconds => seconds + 1);
-      }, 1000);
-    } else if (!isActive && seconds !== 0) {
-      clearInterval(interval);
-    }
-    return () => clearInterval(interval);
-  }, [isActive, seconds]);
 
   return (
     <div className="container">
@@ -45,17 +22,7 @@ function App() {
 
             <div className="home__score">{score}</div>
           </div>
-            <div className="app">
-              <div className="timer">{seconds}</div>
-              <div className="row">
-              <button className={`button button-primary button-primary-${isActive ? 'active' : 'inactive'}`} onClick={toggle}>
-                {isActive ? 'Pause' : 'Start'}
-              </button>
-              <button className="button" onClick={reset}>
-                Reset
-              </button>
-            </div>
-    </div>
+          <div className="timer">00:00</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score">{awayScore}</div>
@@ -66,13 +33,16 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button onClick ={() => setScore (score + 7)}>Home Touchdown</button>
-         <span></span>
-          <button onClick={() => setScore(score + 3)}>Home Field Goal</button>
+
+          <button className="homeButtons__touchdown" onClick ={() => 
+            setScore (score + 7)}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick ={() => 
+            setScore(score + 3)}>Home Field Goal</button>
+            
         </div>
         <div className="awayButtons">
-          <button onClick={() => setAwayScore(awayScore + 7)}>Away Touchdown</button>
-          <button onClick={() => setAwayScore(awayScore + 3)}>Away Fieldgoal</button>
+          <button className="awayButtons__touchdown">Away Touchdown</button>
+          <button className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
       </section>
     </div>
